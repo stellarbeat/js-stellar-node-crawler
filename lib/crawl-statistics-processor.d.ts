22e0a6b0de5276b9247834604bb4342132c17fe8
pieterjan84 <pieterjan84@users.noreply.github.com>
2019-04-16 20:32:44 +0200

    removed lib from git

--- lib/crawl-statistics-processor.d.ts
+++
@@ -1,6 +0,0 @@
-import { Node, Network } from "@stellarbeat/js-stellar-domain";
-declare const _default: {
-    updateNodeStatistics: (node: Node) => void;
-    updateNetworkStatistics: (network: Network) => void;
-};
-export default _default;
--- lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { Crawler } from './crawler';
-export { default as jsonStorage } from './json-storage';
