c901d1bbce336be5aa631d36033e05c79314d0ae
pieterjan84 <pieterjan84@users.noreply.github.com>
2023-08-28 11:21:55 +0200

    updated pino

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Crawler, CrawlerConfiguration } from './crawler';
-import * as P from 'pino';
+import {pino} from 'pino';
 import { QuorumSetManager } from './quorum-set-manager';
 import { ScpManager } from './scp-manager';
 import { createNode } from '@stellarbeat/js-stellar-node-connector';
@@ -10,10 +10,10 @@ export { default as jsonStorage } from './json-storage';
 
 export function createCrawler(
 	config: CrawlerConfiguration,
-	logger?: P.Logger
+	logger?: pino.Logger
 ): Crawler {
 	if (!logger) {
-		logger = P({
+		logger = pino({
 			level: process.env.LOG_LEVEL || 'info',
 			base: undefined
 		});
