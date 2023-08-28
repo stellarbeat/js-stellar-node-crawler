f2b0dfd2efbf3084956a837b9ce5ee5d1c2f462e
pieterjan84 <pieterjan84@users.noreply.github.com>
2023-08-28 11:26:59 +0200

    updated dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,10 @@ module.exports = {
 		'prettier'
 	],
 	rules: {
-		'@typescript-eslint/ban-ts-comment': 'off'
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-unsafe-declaration-merging': 'off',
 	},
 	env: {
 		node: true
